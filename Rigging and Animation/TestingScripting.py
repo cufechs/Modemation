Fixed right shoulder rotation on body + simple fix in head XY plane rotation

Rigging and Animation/TestingScripting.py
@@ -53,6 +53,7 @@ class cf(): # common functions
             for joint in cf.JOINTS_MAP:
                 if frames[i][joint][-1] == 0:
                     frames[i][joint] = frames[i-1][joint]
+                    frames[i][joint][-1] = 0
         return frames
 
     @staticmethod
@@ -72,6 +73,9 @@ class cf(): # common functions
 
     @staticmethod
     def getDepthAngle(L1, L2, degree=False):
+        if L2/L1 > 1:
+            return 0
+        
         angle = math.acos(L2/L1)
         if degree:
             angle = math.degrees(angle)
@@ -103,11 +107,19 @@ class test(bpy.types.Operator):
         bpy.ops.object.mode_set(mode='POSE', toggle=False)
         bones = scn.objects['Armature'].pose.bones
         
-        angle_left = cf.getAngle_2pts(list(bones['ShoulderLeft'].head)[:-1], list(bones['ShoulderLeft'].tail)[:-1])
-        angle_right = cf.getAngle_2pts(list(bones['ShoulderRight'].head)[:-1], list(bones['ShoulderRight'].tail)[:-1])
+        frames, fps = cf.load_frames_pose()
+        fps = round(fps)
         
-        print('left:', np.rad2deg(angle_left))
-        print('right:', np.rad2deg(angle_right))
+        ff = 7
+        
+        print('REye', frames[ff]['REye'])
+        print('LEye', frames[ff]['LEye'])
+        print('Nose', frames[ff]['Nose'])
+        frame = frames[ff]
+        
+        angle_diff = cf.getAngle_2pts(frame['REar'][:-1], frame['LEar'][:-1])
+        print('angle_diff:', np.rad2deg(angle_diff))
+        
         
         return {"FINISHED"}
 
@@ -178,39 +190,95 @@ class animate(bpy.types.Operator):
         ###################################################################
         bones_map_shoulder = [['ShoulderLeft', 'LShoulder', 'LElbow', 'ShoulderLeftBone', -1],
                             ['ShoulderRight', 'RShoulder', 'RElbow', 'ShoulderRightBone', 1]]
-        arm_angle_threshold = -87
-        for x1,x2,x3,x4,x5 in bones_map_shoulder:
-            raised = False
-            for frame_num in range(len(frames)-1):
-                bones[x1].bone.select = True  
-                bpy.context.scene.frame_set((frame_num+1)*(24//fps) + 1)
-                
-                angle_diff = cf.getAngle_2pts(frames[frame_num+1][x2][:-1], frames[frame_num+1][x3][:-1]) - cf.getAngle_2pts(frames[frame_num][x2][:-1], frames[frame_num][x3][:-1])
-                
-                angle_model = cf.getAngle_2pts(list(bones['ShoulderLeft'].head)[:-1], list(bones['ShoulderLeft'].tail)[:-1])
-                if np.rad2deg(angle_model + angle_diff) < arm_angle_threshold and not raised:
-                    raised = True
-                    
-                    bones[x1].bone.select = True 
-                    bpy.ops.transform.rotate(value=0.3, orient_axis='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
-                    
-                    bones[x1].bone.select = False
-                    bones[x4].bone.select = True 
-                    bpy.ops.transform.rotate(value=0.4*x5, orient_axis='Z', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, False, True), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
+        arm_angle_threshold_l = -87
+        arm_angle_threshold_r = -93
+        
+        x1_l,x2_l,x3_l,x4_l,x5_l = bones_map_shoulder[0]
+        x1_r,x2_r,x3_r,x4_r,x5_r = bones_map_shoulder[1]
+        raised_r = False
+        raised_l = False
+        
+        for frame_num in range(len(frames)-1):
+            bpy.context.scene.frame_set((frame_num+1)*(24//fps) + 1)
+            
+            ####### Left Arm Animation ########################################
+            bones[x1_l].bone.select = True  
+            
+            angle_diff = cf.getAngle_2pts(frames[frame_num+1][x2_l][:-1], frames[frame_num+1][x3_l][:-1]) - cf.getAngle_2pts(frames[frame_num][x2_l][:-1], frames[frame_num][x3_l][:-1])            
+            angle_model = cf.getAngle_2pts(list(bones[x1_l].head)[:-1], list(bones[x1_l].tail)[:-1])
 
-                    bones[x4].bone.select = False
-                    bones[x1].bone.select = True 
+            if np.rad2deg(angle_model + angle_diff) < arm_angle_threshold_l and not raised_l:                    
+                bones[x1_l].bone.select = True 
+                bpy.ops.transform.rotate(value=0.3, orient_axis='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
                 
-                elif np.rad2deg(angle_model + angle_diff) > arm_angle_threshold and raised:
-                    bpy.ops.transform.rotate(value=-0.3, orient_axis='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
-                    raised = False
+                bones[x1_l].bone.select = False
+                bones[x4_l].bone.select = True 
+                bpy.ops.transform.rotate(value=0.4*x5_l, orient_axis='Z', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, False, True), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
+
+                bones[x4_l].bone.select = False
+                bones[x1_l].bone.select = True 
+                raised_l = True
                 
-                bpy.ops.transform.rotate(value=angle_diff, orient_axis='Y', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, True, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
+            elif np.rad2deg(angle_model + angle_diff) < arm_angle_threshold_l and raised_l:
+                bones[x1_l].bone.select = False
+                bones[x4_l].bone.select = True 
+                bpy.ops.transform.rotate(value=0.4*x5_l, orient_axis='Z', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, False, True), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
+
+                bones[x4_l].bone.select = False
+                bones[x1_l].bone.select = True 
+            
+            elif np.rad2deg(angle_model + angle_diff) > arm_angle_threshold_l and raised_l:
+                bpy.ops.transform.rotate(value=-0.3, orient_axis='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
+                raised_l = False
+            
+            bpy.ops.transform.rotate(value=angle_diff, orient_axis='Y', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, True, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
+            
+            bones[x4_l].bone.select = True 
+            bpy.ops.anim.keyframe_insert_menu(type='Rotation')
+            bones[x4_l].bone.select = False
+            bones[x1_l].bone.select = False
+            
+            ####### Right Arm Animation ########################################
+        
+            bones[x1_r].bone.select = True
+            
+            angle_diff = cf.getAngle_2pts(frames[frame_num+1][x2_r][:-1], frames[frame_num+1][x3_r][:-1]) - cf.getAngle_2pts(frames[frame_num][x2_r][:-1], frames[frame_num][x3_r][:-1])
+            angle_model = cf.getAngle_2pts(list(bones[x1_r].head)[:-1], list(bones[x1_r].tail)[:-1])
+            
+            if np.rad2deg(angle_model + angle_diff) > arm_angle_threshold_r and np.rad2deg(angle_model + angle_diff) < 0 and not raised_r:
+                bones[x1_r].bone.select = True 
+                bpy.ops.transform.rotate(value=0.3, orient_axis='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
                 
-                bones[x4].bone.select = True 
-                bpy.ops.anim.keyframe_insert_menu(type='Rotation')
-                bones[x4].bone.select = False
-                bones[x1].bone.select = False
+                bones[x1_r].bone.select = False
+                bones[x4_r].bone.select = True 
+                bpy.ops.transform.rotate(value=0.4*x5_r, orient_axis='Z', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, False, True), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
+
+                bones[x4_r].bone.select = False
+                bones[x1_r].bone.select = True 
+                raised_r = True
+                
+            elif np.rad2deg(angle_model + angle_diff) > arm_angle_threshold_r and np.rad2deg(angle_model + angle_diff) < 0 and raised_r:
+                bones[x1_r].bone.select = False
+                bones[x4_r].bone.select = True 
+                bpy.ops.transform.rotate(value=0.4*x5_r, orient_axis='Z', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, False, True), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
+
+                bones[x4_r].bone.select = False
+                bones[x1_r].bone.select = True 
+            
+            elif (np.rad2deg(angle_model + angle_diff) < arm_angle_threshold_r or np.rad2deg(angle_model + angle_diff) > 0) and raised_r:
+                bpy.ops.transform.rotate(value=-0.3, orient_axis='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
+                raised_r = False
+            
+            
+            if frame_num == 7:
+                print (np.rad2deg(angle_diff))
+            
+            bpy.ops.transform.rotate(value=angle_diff, orient_axis='Y', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, True, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
+            
+            bones[x4_r].bone.select = True 
+            bpy.ops.anim.keyframe_insert_menu(type='Rotation')
+            bones[x4_r].bone.select = False
+            bones[x1_r].bone.select = False
         ###################################################################
         
         
@@ -267,7 +335,7 @@ class animate(bpy.types.Operator):
 
             # Y-axis rotation
             angle_diff = cf.getAngle_2pts(frame['REar'][:-1], frame['LEar'][:-1])
-            if abs(angle_diff) > math.radians(5):
+            if abs(angle_diff) > math.radians(5) and frame['REar'][-1] != 0 and frame['LEar'][-1] != 0:
                 bpy.ops.transform.rotate(value=angle_diff, orient_axis='Y', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, True, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
 
             # Z-axis rotation 
@@ -364,7 +432,7 @@ class animate(bpy.types.Operator):
                 
                 Angle = 0 if Angle < 0 else Angle
             
-                if Angle > thighAngleThreshold: 
+                if Angle > shoulderAngleThreshold: 
                     bones[bones_map_arms[i][0]].bone.select = True
                     bpy.context.scene.frame_set((frame_num)*(24//fps) + 1)
                     bpy.ops.transform.rotate(value=Angle * bones_map_arms[i][3], orient_axis='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)